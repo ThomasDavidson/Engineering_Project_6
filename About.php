c81072b82d4540b81b91c63827e42e708812d6ae
Iambob4ever <bob.boettcher@hotmail.com>
2019-06-10 14:11:04 -0400

    Update About.php
    basically just got up to ass 1 requirements

--- About.php
+++ About.php
@@ -56,16 +56,55 @@
             <div class="col-md-10" style='background-color: white'>	
             <h1>About us</h1>
             <img src="images/team photo.jpg" alt= "team photo">
-            <ul>
-                <li>Our group consists of 4 members: Robert, Damian, Stephan and Thomas. Our mascot is Haron and our website features him.</li>
-            
-            </ul>
-        <!-- Footer Section --> 
+            <div>Our group consists of 4 members: Robert, Damian, Stephan and Thomas. Our mascot is Haron and our website features him.</div>
+ 
+            <div>Bellow is a map of our school </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2899.52587161604!2d-80.39901898451085!3d43.386936879131355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882b8a7ec9d60715%3A0xd5e712873de8af2d!2sConestoga+College+-+Cambridge+Campus!5e0!3m2!1sen!2sca!4v1560189680447!5m2!1sen!2sca" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+  	
+
+            </div>
+						
+		<!-- Second Section, This Column Width Smaller --> 
+			<div class="col-md-2" style='background-color:white'>
+			<div class="row">
+			<div class="col-md-12">
+							
+		<!-- Load news.php -->
+						
+			<?php include('news.php');?>
+						
+			</div>
+			</div>
+							
+							<!-- Scond Section, Part Two ---> 
+							<div class="row">
+								<div class="col-md-12" style='background-color: white'>
+								<h1>Useful Links</h1>
+								<ul>
+								  <li><a href="#">Our Youtube Channel</a></li>
+								  <li><a href="#">Our Github Repository</a></li>
+								  <li><a href="#">link 3</a></li>
+								  <li><a href="#">link 4</a></li>
+								</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					
+				<!-- Footer Section --> 
 					<div class="row">
-					<div class="col-md-12">
-					<div class="container-bg">
-			<!-- Footer Code with start year -->
-						<p class="text-light text-center">&copy; Copyright 2014-<script>document.write(new Date().getFullYear())</script></p>
-
-        <body>
-
+						<div class="col-md-12">
+							<div class="container-bg">
+							<!-- Footer Code with start year -->
+							<p class="text-light text-center">&copy; Copyright 2014-<script>document.write(new Date().getFullYear())</script></p>
+							</div>
+						</div>			
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	</div>
+  </body>
+</html>
