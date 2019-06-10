fa8f50d3cc3ce4de25292c3494481f6c055cac69
Iambob4ever <bob.boettcher@hotmail.com>
2019-06-10 12:31:51 -0400

    Update About.php
    setup using standard template

---
+++ About.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>About</title>
+
+	<!-- Link Bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	
+	<!-- For Social Media Icons -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">  
+	
+	<!-- Link Custom CSS -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	
+	<!-- Link JS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+  </head>
+
+  <body>
+	
+	<!-- Container has margins but container-fluid does not -->
+	<div class="container-fluid">
+		<div class="container-bg">
+
+		<!-- Top of the Page --> 
+			<div class="row" >
+		<!-- Container-bg to set background color --> 
+				<div class="col-md-12">
+					<div class="page-header">
+						<br>
+						<h1 class="text-light"><img src="Images/CC_logo.jpg" alt="Conestoga College Logo" height="60" wide="60"> Engineering Project 6</h1>
+						<h5 class="text-light">By: Thomas, Damien, Robert & Steve</h5>
+					</div>
+					</div>
+			</div>
+			<br>
+			
+			<div class="row">
+				<div class="col-md-12">	
+
+			<!-- Load the Navigation Bar wit hPHP include function --> 
+					<?php 
+					include('menu.php');
+					?>
+						
+				</div>
+			</div> 
