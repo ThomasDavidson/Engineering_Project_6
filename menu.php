38a4d4ea3c0497bdc5cdbdd9c01eca630e627c28
DamianScarpone <damian_s_66@hotmail.com>
2019-06-10 12:54:01 -0400

    fixed link,moved form

--- menu.php
+++ menu.php
@@ -7,7 +7,7 @@
 	</li>
 
 	<li class="nav-item">
-		<a class="nav-link disabled" href="form.php" target="_blank">Sign In</a>
+		<a class="nav-link" href="form.php" target="_blank">Sign In</a>
 	</li>
 	
 	<li class="nav-item">
