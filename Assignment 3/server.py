ecafa4daf93383cd8437599e0aac54b6bd6949cb
Iambob4ever <bob.boettcher@hotmail.com>
2019-07-04 18:39:55 -0400

    Update server.py

--- Assignment 3/server.py
+++ Assignment 3/server.py
@@ -14,61 +14,62 @@ def Main():                                     # define the main function
     mySocket.bind((host, port))                 # bind take a tuple as input (bind the host and port to the socket created above) 
 
     mySocket.listen(1)                          # 1 means perpetually listen until the connection is closed
-    conn, addr = mySocket.accept() 
-                 # conn and addr hold the connection and address of the client
+    conn, addr = mySocket.accept()              # conn and addr hold the connection and address of the client
     
     print("Connection from: " + str(addr))  # print the clients 
     while True:
-        data = conn.recv(1024).decode()         # decode() necessary in python 3
-        if not data:
+        ID_reqeust = conn.recv(1024).decode()         # decode() necessary in python 3
+        if not ID_reqeust:
             break
         
-        print("from connected user: " + str(data))
-        if data == '1':
-            with open(path, 'r') as database:
+        print("ID from connected user: " + str(ID_reqeust))
+        
+        with open(path, 'r') as database:
                 data = csv.DictReader(database, delimiter=',')
-                         
-                for row in data:                     
-                    current_data = row['ID']
-                    current_data = current_data +','
-                    conn.send(current_data.encode())
-                   
-                    current_data = row['Name']
-                    current_data = current_data +','
-                    conn.send(current_data.encode())
+                for row in data:
+                   if row['ID'] == ID_reqeust: #increments through the database till it finds the right ID
+                        if row['ID'] is None:
+                            print("ID not found")       #checks  not out of bounds
+                        current_data = row['ID']
+                        current_data = current_data +','
+                        conn.send(current_data.encode())
                     
-                    current_data = row['Last Name']
-                    current_data = current_data +','
-                    conn.send(current_data.encode())
+                        current_data = row['Name']
+                        current_data = current_data +','
+                        conn.send(current_data.encode())
+                        
+                        current_data = row['Last Name']
+                        current_data = current_data +','
+                        conn.send(current_data.encode())
 
-                    current_data = row['Street#']
-                    current_data = current_data +','
-                    conn.send(current_data.encode())
+                        current_data = row['Street#']
+                        current_data = current_data +','
+                        conn.send(current_data.encode())
+                        
+                        current_data = row['Street Name']
+                        current_data = current_data +','
+                        conn.send(current_data.encode())
+                        
+                        current_data = row['City']
+                        current_data = current_data +','
+                        conn.send(current_data.encode())
+                        
+                        current_data = row['Postal']
+                        current_data = current_data +','
+                        conn.send(current_data.encode())
+                        
+                        current_data = row['Province']
+                        current_data = current_data +','
+                        conn.send(current_data.encode())
                     
-                    current_data = row['Street Name']
-                    current_data = current_data +','
-                    conn.send(current_data.encode())
-                    
-                    current_data = row['City']
-                    current_data = current_data +','
-                    conn.send(current_data.encode())
-                    
-                    current_data = row['Postal']
-                    current_data = current_data +','
-                    conn.send(current_data.encode())
-                    
-                    current_data = row['Province']
-                    current_data = current_data +','
-                    conn.send(current_data.encode())
-                  
-                    current_data = row['Country']
-                    current_data = current_data +','
-                    conn.send(current_data.encode())
+                        current_data = row['Country']
+                        current_data = current_data +','
+                        conn.send(current_data.encode())
                    
-                    breaker = conn.recv(1024).decode()         #checks to see if user wants to load another row
-                    breaker = breaker.upper()
-                    if breaker == 'N':
-                        break
+                breaker = conn.recv(1024).decode()         #checks to see if user wants to load another row
+                breaker = breaker.upper()
+                if breaker == 'N':
+                    break
     conn.close()
 
 if __name__ == '__main__':
