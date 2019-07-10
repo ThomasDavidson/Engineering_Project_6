66d808d1d3e55cf616175d42470d692001778399
Iambob4ever <bob.boettcher@hotmail.com>
2019-07-10 16:12:55 -0400

    Update logout.php
    protected the page.

--- logout.php
+++ logout.php
@@ -1,7 +1,22 @@
 <?php
-
-    session_start();
-    session_destroy();
-
-    echo"you're now logged out";
+    if(isset($_SESSION['username']))
+    {
+        if(($_SESSION['username'] != Bob))
+        {
+            echo "You are not logged in";
+            echo "<a href='logout.php'>Click to return home</a>";
+        }
+        else
+        {
+            session_start();
+            session_destroy();
+            echo"you're now logged out\n";
+            echo "<a href='index.php'>Click to return home</a>";
+        } 
+    }
+    else
+    {
+        echo "You are not logged in";
+        echo "<a href='index.php'>Click to return home</a>";
+    }
 ?>
