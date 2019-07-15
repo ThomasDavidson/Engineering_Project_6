4b563871456cd5d0f357bb12f7febf417fcbc20f
Iambob4ever <bob.boettcher@hotmail.com>
2019-07-15 16:16:31 -0400

    Create controller.php

---
+++ controller.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Control Panel</title>
+        <link rel="stylesheet" type="text/css" href="css/controller.css">
+        <div style background-image url(Images/logo.jpg); </div>
+    </head>
+
+    <body>
+        <div id="current" > Current Floor </div> 
+        <div id="floor"> First Floor </div>
+        <div id="floor"> Second Floor </div>
+        <div id="floor"> Third Floor </div>
+    </body>
+
+</html>
