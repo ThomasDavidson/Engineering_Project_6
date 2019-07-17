d03858248aafdb53ad2418529cafec2184bc384a
Iambob4ever <bob.boettcher@hotmail.com>
2019-07-17 13:06:16 -0400

    Create converter.php

---
+++ Assignment 3/Code base/converter.php
@@ -0,0 +1,15 @@
+class Controller{
+    private $model;
+    public function __construct(Model $model){
+        $this->model = $model;
+    } 
+}
+
+public function set($amount)
+
+
+
+class Model{
+    private
+
+}
