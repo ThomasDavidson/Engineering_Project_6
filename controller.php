28806ee0d7b153d16922a7013ae3603f94c21628
shano0 <40350482+srhan00@users.noreply.github.com>
2019-08-08 21:52:19 -0400

    Update controller.php

--- controller.php
+++ controller.php
@@ -1,20 +1,80 @@
 <html>
+  
+    <head>
+    <!-- Link Bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	
+	<!-- For Social Media Icons -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">  
+	
+	<!-- Link Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
+	
+	<!-- Link JS -->
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    
+        <title>Forms: Sign in</title>
+        <meta name="description" content="This is the Sign in page" />
+		<meta name="robots" content="noindex nofollow" />  <!-- do not want page or any of its links to be indexed -->
+		<meta http-equiv="author" content="Stephen Han" />
+        <meta http-equiv="pragma" content="no-cache" /> <!-- want browser to reload this page every time -->
 
+    </head>
+    <body>
+    <!-- Container has margins but container-fluid does not -->
+	<div class="container-fluid">
+		<div class="container-bg">
+
+		<!-- Top of the Page --> 
+			<div class="row" >
+			
+		<!-- Container-bg to set background color --> 
+				<div class="col-md-12">
+					<div class="page-header">
+						<br>
+						<h1 class="text-light"><img src="../Images/CC_logo.jpg" alt="Conestoga College Logo" height="60" wide="60"> Engineering Project 6</h1>
+						<h5 class="text-light">By: Thomas, Damian, Robert & Steve</h5>
+					</div>
+				</div>
+			</div>
+			<br>
+			
+			<div class="row">
+				<div class="col-md-12">	
+
+			<!-- Load the Navigation Bar wit hPHP include function --> 
+					<?php 
+					include('menu.php');
+					?>
+						
+				</div>
+            </div> 
+            
+    <!-- Main Section --> 
+	<div class="col-md-12" style='background-color: white'>	
+
+     <h1 class="text-muted">Members Only Page</h1>
+	
+	<div>
+	
 	<?php
 	// connect to db
 	include 'php/database.php';
 	
 	session_start();
 
-	$conn = new database('192.168.0.200', 'elevator', 'root', '3Yn4$zT&');
+	$conn = new database('127.0.0.1', 'elevator', 'root', '3Yn4$zT&');
 	$conn->dbConnect();
 
 	// tell user what floor it's on 
 	if (isset($_SESSION['username'])) { 
-    $current = $db->query("Select currentFloor FROM elevatorNetwork");
-    echo "The car is now on floor: ".$_POST["floor"];
-    echo "<br />";
 	
+		$current = $db->query("Select currentFloor FROM elevatorNetwork");
+		echo "The car is now on floor: ".$_POST["floor"];
+		echo "<br />";
+		
 	}
 	
 	else { 
@@ -25,14 +85,28 @@
 	
 	?>
 	
+		
     <form action= "changeController.php" method="post" id="floor"> 
     <p>Enter a floor number to send the elevator to 
         <input type="number" name="floorNumber" min="1" max="3"/>
         <input type="submit" value="Send It"/>
     </p>
-
 	
+	
+	</div>
+       
+		<div class="row">
+			<div class="col-md-12 text-center">
+                            <footer class="text-light" id='footer'></footer>
+							<script src="../js/DateObjectModel.js"></script>
+                            </div>
+                        </div>
+                     </div>
+		  </div>
+</body>
+    
 </html>
+
 
 
  
